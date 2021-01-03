d943cdfa6b9391c356d3cff1b3b1171295fdf8b6
scratcher-402 <75668082+scratcher-402@users.noreply.github.com>
2021-01-03 15:16:30 +0300

    Update zoom.sh

--- zoom.sh
+++ zoom.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-wget https://github.com/scratcher-402/supm-offical-repo/raw/main/zoom.tar.xz.aaa https://github.com/scratcher-402/supm-offical-repo/raw/main/zoom.tar.xz.aab
-cat zoom.tar.xz.aaa zoom.tar.xz.aab > scratux.tar.xz
-rm zoom.tar.xz.aaa zoom.tar.xz.aab
-xz -d zoom.tar.xz
+echo 'Package deleted, press Ctrl+C'
