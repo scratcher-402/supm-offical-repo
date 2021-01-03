43ba2c2b71fddb488b626258d21731d809d02d5b
scratcher-402 <75668082+scratcher-402@users.noreply.github.com>
2021-01-03 13:40:27 +0300

    Update zoom.sh

--- zoom.sh
+++ zoom.sh
@@ -2,4 +2,4 @@
 wget https://github.com/scratcher-402/supm-offical-repo/raw/main/zoom.tar.xz.aaa https://github.com/scratcher-402/supm-offical-repo/raw/main/zoom.tar.xz.aab
 cat zoom.tar.xz.aaa zoom.tar.xz.aab > scratux.tar.xz
 rm zoom.tar.xz.aaa zoom.tar.xz.aab
-xz -d scratux.tar.xz
+xz -d zoom.tar.xz
